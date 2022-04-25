8e653243d28ba9229f8c43eae6e23543e5d85eee
Gustavo Valenzuela <GALTdea@gmail.com>
2022-04-25 12:26:59 -0700

    separated petri_dish generator and added more documentage to USAGE document, does request_speciment and query_speciment need to have their own resource?

--- lib/generators/petri_dish/query_specimen_generator.rb
+++ lib/generators/petri_dish/query_specimen_generator.rb
@@ -1,7 +1,9 @@
 module PetriDish
   class QuerySpecimenGenerator < Rails::Generators::Base
+    # create_inizializer_file method creates a file named config/initializers/petri_dish.rb if file doesn't exist, otherwise it appends to it.
+    #content: comes from rails active support instrumentation instrumentation documentation. no templet is used.
     def create_initializer_file
-      destination_file = "config/initializers/petri_dish.rb"
+      destination_file = "config/initializers/petri_dish/query_speciments.rb"
       contents = <<~END
         ActiveSupport::Notifications.subscribe "sql.active_record" do |*args|
           PetriDish::QuerySpecimen.prepare_slide(args)
--- lib/generators/petri_dish/request_specimen_generator.rb
+++ lib/generators/petri_dish/request_specimen_generator.rb
@@ -1,7 +1,7 @@
 module PetriDish
   class RequestSpecimenGenerator < Rails::Generators::Base
     def create_initializer_file
-      create_file "config/initializers/petri_dish.rb",
+      create_file "config/initializers/petri_dish/request_speciments.rb",
       <<~END
       ActiveSupport::Notifications.subscribe "process_action.action_controller" do |*args|
         PetriDish::RequestSpecimen.prepare_slide(args)
